e3e89d055341f42a633984e5973ca15f909c6eaa
Rashi Dhar <rdhar1997@gmail.com>
2020-04-06 13:42:41 -0400

    feat: add placeholders for about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,47 @@
 <template>
   <div>
-    <div class="hero-body">
-      <div class="hero-left-text">
-        <div class="hero-left-title">
-          Search, Filter on tags.
+    <div class="about-body">
+      <div class="about-right-text">
+        <div class="about-right-title">
+          {{ $t("about.explanation.title") }}
         </div>
-        <div class="hero-left-subtext">
-          Every behavioral task repository comes with a search bar and advanced
-          filter management.
+        <div class="about-right-subtext">
+          {{ $t("about.explanation.description") }}
+        </div>
+      </div>
+    </div>
+
+    <div class="about-body">
+      <div class="about-left-text">
+        <div class="about-left-title">
+          {{ $t("about.register.title") }}
+        </div>
+        <div class="about-left-subtext">
+          {{ $t("about.register.description") }}
+        </div>
+      </div>
+    </div>
+
+    <div class="about-body">
+      <div class="about-right-text">
+        <div class="about-right-title">
+          {{ $t("about.task-starter-description.title") }}
+        </div>
+        <div class="about-right-subtext">
+          {{ $t("about.task-starter-description.description") }}
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script></script>
-//
-<style lang="sass">
-// .
-//
-</style>
+<script>
+import "@/styles/themes/default/components/_about.sass";
+export default {
+  name: "About",
+  components: {},
+  computed: {},
+  methods: {},
+  mounted() {}
+};
+</script>
